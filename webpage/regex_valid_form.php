ad23df3d2c28fdb90e54a38b13e4d1c8ce74dec5
sukhrobbek <33787533+sukhrobbek@users.noreply.github.com>
2019-03-27 12:50:01 +0500

    Update regex_valid_form.php

--- webpage/regex_valid_form.php
+++ webpage/regex_valid_form.php
@@ -1,26 +1,20 @@
 <?php
-
 	$pattern="";
 	$text="";
 	$replaceText="";
 	$replacedText="";
-
 	$match="Not checked yet.";
-
 if ($_SERVER["REQUEST_METHOD"]=="POST") {
 	$pattern=$_POST["pattern"];
 	$text=$_POST["text"];
 	$replaceText=$_POST["replaceText"];
-
 	$replacedText=preg_replace($pattern, $replaceText, $text);
-
 	if(preg_match($pattern, $text)) {
-						$match="Match!";
-					} else {
-						$match="Does not match!";
-					}
+		$match="Match!";
+	} else {
+		$match="Does not match!";
+	}
 }
-
 ?>
 
 
